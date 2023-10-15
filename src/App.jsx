af4a1a735aa31a5db694d13b1a7689f77845d9ea
Athul Vijayan <web.wizard.athul@gmail.com>
2023-10-16 00:12:30 +0530

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,12 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  var mainContainer = document.getElementById("mainContainer");
-  const [color, setColor] = useState("");
-
-  var bgColorChange = (newColor) => {
-    setColor(newColor);
-    if (mainContainer) {
-      mainContainer.style.backgroundColor = newColor;
-    }
-  };
-
-  var btns = document.querySelectorAll("button");
-  btns.forEach((btn) => {
-    btn.innerHTML = btn.innerHTML.toUpperCase();
-  });
+  
 
   console.log(color);
   return (
     <>
-    wizard
+    <h1 className="text-4xl text-center text-white"> wizard</h1>
     </>
   );
 }
